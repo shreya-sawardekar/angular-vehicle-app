cb1e4b6dc29861d25a7f97619d4012508e0d8641
pix1289 <shreya.1289@gmail.com>
2025-10-03 21:51:29 +0530

    Add vehicle list

--- app/src/app/vehicle-list/vehicle-list.component.ts
+++ app/src/app/vehicle-list/vehicle-list.component.ts
@@ -3,8 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'vhapp-vehicle-list',
   templateUrl: './vehicle-list.component.html',
-  styleUrls: ['./vehicle-list.component.css']
+  styleUrls: ['./vehicle-list.component.css'],
 })
 export class VehicleListComponent {
-
+  vehicleList = [
+    {
+      id: 1,
+      name: 'Sand Crawler',
+    },
+    {
+      id: 2,
+      name: 'T-16 skyhopper',
+    },
+    {
+      id: 3,
+      name: 'X-34 landspeeder',
+    },
+    {
+      id: 4,
+      name: 'TIE/LN starfighter',
+    },
+    {
+      id: 5,
+      name: 'Snowspeeder',
+    },
+    {
+      id: 6,
+      name: 'TIE bomber',
+    },
+    {
+      id: 7,
+      name: 'AT-AT',
+    },
+    {
+      id: 8,
+      name: 'AT-ST',
+    },
+    {
+      id: 9,
+      name: 'Storm IV Twin-Pod cloud car',
+    },
+    {
+      id: 10,
+      name: 'Sail barge',
+    },
+  ];
 }
